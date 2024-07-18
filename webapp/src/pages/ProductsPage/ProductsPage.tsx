acfc416a92b504d8d152fc46d0d9a0870fe57358
Sindhuja Govindarjan <srin2258@gmail.com>
2024-07-18 19:14:23 -0400

    Added Meta; Dummy Photos

--- webapp/src/pages/ProductsPage/ProductsPage.tsx
+++ webapp/src/pages/ProductsPage/ProductsPage.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
 import PageWrapper from "../PageWrapper";
 import ProductCard from "../../components/ProductCard/ProductCard";
 
-interface Product {
+interface ActiveProduct {
   ProductID: number;
   ProductName: string;
   ProductPhotoURL: string;
-  ProductStatus: string;
 }
 
 const ProductsPage: React.FC = () => {
@@ -16,35 +16,44 @@ const ProductsPage: React.FC = () => {
       Instead of modifying the data locally we want to do it serverside via a post
       request
   */
-  const [products, setProducts] = useState<Product[]>([]);
+  const [activeProducts, setActiveProducts] = useState<ActiveProduct[]>([]);
+  const products = [
+    {
+      ProductID: 1,
+      ProductName: "Ant-Man and the Wasp: Quantumania (Marvel Studios)",
+      ProductPhotoURL:
+        "https://i.vimeocdn.com/video/1625345672-870e3bd5cf157f98e03717b654878072d7b573d07d5319d07bf504ea1b97e6c8-d?mw=900&mh=507&q=70",
+      ProductStatus: "Active",
+    },
+    {
+      ProductID: 2,
+      ProductName: "Master Showreel",
+      ProductPhotoURL:
+        "https://i.vimeocdn.com/video/1836529810-e1db005ef64f60d148ce42dd48925cee4c5b5593e987790ae74c040c471d0ae9-d?mw=900&mh=507&q=70",
+      ProductStatus: "Inactive",
+    },
+  ];
   useEffect(() => {
-    setProducts([
-      {
-        ProductID: 1,
-        ProductName: "dummyPdt1",
-        ProductPhotoURL: "dummyImgUrl1",
-        ProductStatus: "Active",
-      },
-      {
-        ProductID: 2,
-        ProductName: "dummyPdt2",
-        ProductPhotoURL: "dummyImgUrl2",
-        ProductStatus: "Active",
-      },
-    ]);
+    setActiveProducts(
+      products.filter(({ ProductStatus }) => ProductStatus === "Active")
+    );
   }, []);
 
   return (
     <PageWrapper>
       <div className="container mx-auto px-4">
+        <Helmet>
+          <title>Monsters Aliens Robots Zombies VFX - Products</title>
+          <meta name="description" content="MARZ active products" />
+        </Helmet>
         <h1 className="text-2xl font-bold mb-4">Products</h1>
         <div className="grid grid-cols-3 gap-4">
-          {products?.map((product) => (
+          {activeProducts?.map((activeProduct) => (
             <ProductCard
-              key={product.ProductID}
-              id={product.ProductID}
-              name={product.ProductName}
-              imageUrl={product.ProductPhotoURL}
+              key={activeProduct.ProductID}
+              id={activeProduct.ProductID}
+              name={activeProduct.ProductName}
+              imageUrl={activeProduct.ProductPhotoURL}
             />
           ))}
         </div>
