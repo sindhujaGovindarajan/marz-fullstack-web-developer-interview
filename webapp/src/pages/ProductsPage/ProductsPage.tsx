52974469a37daee2a7ffbc4226e981d3de71124b
Sindhuja Govindarjan <srin2258@gmail.com>
2024-07-18 18:21:17 -0400

    feat: dummy product page

--- webapp/src/pages/ProductsPage/ProductsPage.tsx
+++ webapp/src/pages/ProductsPage/ProductsPage.tsx
@@ -1,20 +1,56 @@
-import React from "react";
-import PageWrapper from '../PageWrapper';
+import React, { useEffect, useState } from "react";
+import PageWrapper from "../PageWrapper";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
-const ProductsPage = () => {
+interface Product {
+  ProductID: number;
+  ProductName: string;
+  ProductPhotoURL: string;
+  ProductStatus: string;
+}
+
+const ProductsPage: React.FC = () => {
   /*
     TODO:
       When the drag ends we want to keep the status persistant across logins. 
       Instead of modifying the data locally we want to do it serverside via a post
       request
   */
+  const [products, setProducts] = useState<Product[]>([]);
+  useEffect(() => {
+    setProducts([
+      {
+        ProductID: 1,
+        ProductName: "dummyPdt1",
+        ProductPhotoURL: "dummyImgUrl1",
+        ProductStatus: "Active",
+      },
+      {
+        ProductID: 2,
+        ProductName: "dummyPdt2",
+        ProductPhotoURL: "dummyImgUrl2",
+        ProductStatus: "Active",
+      },
+    ]);
+  }, []);
+
   return (
     <PageWrapper>
-      <h1 className="text-3xl font-bold text-white">
-        Product Page Goes Here
-      </h1>
+      <div className="container mx-auto px-4">
+        <h1 className="text-2xl font-bold mb-4">Products</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {products?.map((product) => (
+            <ProductCard
+              key={product.ProductID}
+              id={product.ProductID}
+              name={product.ProductName}
+              imageUrl={product.ProductPhotoURL}
+            />
+          ))}
+        </div>
+      </div>
     </PageWrapper>
   );
 };
 
-export default ProductsPage
+export default ProductsPage;
